5a035a51ca621a2ecab93c227602d11dac704316
meekn <mitsuharu.kurita@gmail.com>
2014-05-10 22:47:05 +0900

    Add some key binds for helm

--- .emacs.d/inits/20-helm.el
+++ .emacs.d/inits/20-helm.el
@@ -9,3 +9,6 @@
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+
+(define-key global-map (kbd "M-x") 'helm-M-x)
+(define-key global-map (kbd "C-x b") 'helm-buffers-list)
