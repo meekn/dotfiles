19f2b180b2d0cb08dd4a25db53b9e32273b00c3b
meekn <mitsuharu.kurita@gmail.com>
2014-07-11 04:15:53 +0900

    Correct typos in cons

--- .emacs.d/inits/20-helm.el
+++ .emacs.d/inits/20-helm.el
@@ -8,8 +8,8 @@
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 
-(add-to-list 'helm-completing-read-handlers-alist '(find-file nil))
-(add-to-list 'helm-completing-read-handlers-alist '(write-file nil))
+(add-to-list 'helm-completing-read-handlers-alist '(find-file . nil))
+(add-to-list 'helm-completing-read-handlers-alist '(write-file . nil))
 
 (define-key global-map (kbd "M-x") 'helm-M-x)
 (define-key global-map (kbd "C-x b") 'helm-buffers-list)
