f12c7ac94478573633e3abfd6c80e8799fbc992d
meekn <mitsuharu.kurita@gmail.com>
2014-07-11 03:44:03 +0900

    Enable C++11 option

--- .emacs.d/inits/20-flymake.el
+++ .emacs.d/inits/20-flymake.el
@@ -49,7 +49,7 @@
   ;; C++
   (defun flymake-cc-init ()
     (flymake-simple-make-or-generic-init
-     "g++" '("-Wall" "-Wextra" "-pedantic" "-fsyntax-only")))
+     "g++" '("-std=c++11" "-Wall" "-Wextra" "-pedantic" "-fsyntax-only")))
   (push '("\\.\\(cc\\|cpp\\|C\\|CPP\\|hpp\\)\\'" flymake-cc-init)
         flymake-allowed-file-name-masks)
   (add-hook 'c++-mode-hook '(lambda () (flymake-mode t))))
