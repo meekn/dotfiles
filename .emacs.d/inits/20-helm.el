239cc372815b8bf9d4646345816ef9edade04030
meekn <mitsuharu.kurita@gmail.com>
2014-07-11 03:47:52 +0900

    Disable helm for file operations

--- .emacs.d/inits/20-helm.el
+++ .emacs.d/inits/20-helm.el
@@ -6,9 +6,10 @@
 (helm-mode 1)
 
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
-(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
-(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+
+(add-to-list 'helm-completing-read-handlers-alist '(find-file nil))
+(add-to-list 'helm-completing-read-handlers-alist '(write-file nil))
 
 (define-key global-map (kbd "M-x") 'helm-M-x)
 (define-key global-map (kbd "C-x b") 'helm-buffers-list)
