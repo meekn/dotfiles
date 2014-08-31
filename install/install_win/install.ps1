84a66f66b6f28bf3e081fa72bfc2721f61359e3a
meekn <mitsuharu.kurita@gmail.com>
2014-08-31 20:56:17 +0900

    Automate iterations on install scripts

--- install/install_win/install.ps1
+++ install/install_win/install.ps1
@@ -1,13 +1,9 @@
 $script_dir = (Split-Path $script:myInvocation.MyCommand.path -parent)
 
-$command = "iex " + (Join-Path $script_dir "install_ctrl2cap.ps1")
-$args = "-NoProfile", "-ExecutionPolicy", "unrestricted", "-Command", $command
-Start-Process powershell.exe -NoNewWindow -Wait -ArgumentList $args
+$targets = ("install_ctrl2cap.ps1", "install_cygwin.ps1", "install_ntemacs.ps1")
 
-$command = "iex " + (Join-Path $script_dir "install_cygwin.ps1")
-$args = "-NoProfile", "-ExecutionPolicy", "unrestricted", "-Command", $command
-Start-Process powershell.exe -NoNewWindow -Wait -ArgumentList $args
-
-$command = "iex " + (Join-Path $script_dir "install_ntemacs.ps1")
-$args = "-NoProfile", "-ExecutionPolicy", "unrestricted", "-Command", $command
-Start-Process powershell.exe -NoNewWindow -Wait -ArgumentList $args
+$targets | ForEach-Object {
+    $command = "iex " + (Join-Path $script_dir $_)
+    $args = "-NoProfile", "-ExecutionPolicy", "unrestricted", "-Command", $command
+    Start-Process powershell.exe -NoNewWindow -Wait -ArgumentList $args
+}
