f5e58b585f05ae93cc2b9fb382e100da1a5601f5
meekn <mitsuharu.kurita@gmail.com>
2014-08-31 20:47:15 +0900

    Add NoNewWindow option for Start-Process in PowerShell scripts

--- install/install_win/install.ps1
+++ install/install_win/install.ps1
@@ -2,12 +2,12 @@ $script_dir = (Split-Path $script:myInvocation.MyCommand.path -parent)
 
 $command = "iex " + (Join-Path $script_dir "install_ctrl2cap.ps1")
 $args = "-NoProfile", "-ExecutionPolicy", "unrestricted", "-Command", $command
-Start-Process powershell.exe -Verb runas -ArgumentList $args
+Start-Process powershell.exe -NoNewWindow -Wait -ArgumentList $args
 
 $command = "iex " + (Join-Path $script_dir "install_cygwin.ps1")
 $args = "-NoProfile", "-ExecutionPolicy", "unrestricted", "-Command", $command
-Start-Process powershell.exe -Verb runas -ArgumentList $args
+Start-Process powershell.exe -NoNewWindow -Wait -ArgumentList $args
 
 $command = "iex " + (Join-Path $script_dir "install_ntemacs.ps1")
 $args = "-NoProfile", "-ExecutionPolicy", "unrestricted", "-Command", $command
-Start-Process powershell.exe -Verb runas -ArgumentList $args
+Start-Process powershell.exe -NoNewWindow -Wait -ArgumentList $args
