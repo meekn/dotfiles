f5a8abb4bfef3146904010be12663408ab0c9e55
Mitsuharu Kurita <mitsuharu.kurita@gmail.com>
2017-02-06 19:48:10 +0900

    Auto update in init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,3 +25,17 @@
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load "~/.emacs.d/inits")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (jedi-core wgrep company yasnippet wgrep-ag use-package undo-tree projectile magit init-loader helm cython-mode company-jedi anzu ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
